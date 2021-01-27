2472515340d40e25895d58d5ceadfa3ee89e38a3
Velorian92 <43962477+Velorian92@users.noreply.github.com>
2021-01-27 20:05:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let a = document.getElementById('inp1');
+let b = document.getElementById('inp2');
+let btn = document.getElementById('btn');
+let res = document.getElementById('result');
+
+btn.onclick = event => {
+
+    let firstInp = a.value.split(',');
+    let secondInp = b.value.split(' ');
+
+    function search(a, b) {
+        let result = [];
+        for (var akay in a) {
+            let found = false;
+            for (var bkey in b) {
+                if (a[akay] == b[bkey]) {
+                    found = true;
+                    continue;
+                }
+            }
+            if (!found) {
+                result.push(a[akay])
+            }
+        }
+        return result;
+    }
+
+    res.innerHTML = search(secondInp, firstInp)
+
+}
+
