ee9245430f5a29b54423affdc94491db600b0bfb
Velorian92 <43962477+Velorian92@users.noreply.github.com>
2021-01-28 15:44:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let a = document.getElementById('inp1');
 let b = document.getElementById('inp2');
 let btn = document.getElementById('btn');
 let res = document.getElementById('result');
+let wrong = document.getElementById('wrong');
+
 
 btn.onclick = event => {
 
@@ -9,22 +11,39 @@ btn.onclick = event => {
     let secondInp = b.value.split(/[\s,;.:]+/);
 
     function search(a, b) {
-        let result = [];
-        for (var akay in a) {
-            let found = false;
-            for (var bkey in b) {
-                if (a[akay] == b[bkey]) {
-                    found = true;
-                    continue;
-                }
-            }
-            if (!found) {
-                result.push(a[akay])
-            }
-        }
-        return result;
-    }
 
+        let result = [];
+        let wrong = [];
+
+        for (var akay in a) {
+        
+            let found = false;
+        
+            for (var bkey in b) {
+        
+                if (a[akay] == b[bkey]) {
+        
+                    found = true;
+
+                    continue;
+        
+                }
+        
+            }
+        
+            if (!found) {
+        
+                result.push(a[akay])
+                
+            }
+        
+        }
+
+        return result;
+    
+    }
+    
+    wrong.innerHTML = search(firstInp,secondInp)
     res.innerHTML = search(secondInp, firstInp)
 
 }
